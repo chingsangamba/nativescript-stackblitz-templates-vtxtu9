8ca85cf73a30db745237947888749fb61cdb2ad9
chingsangamba <69503943+chingsangamba@users.noreply.github.com>
2022-12-27 10:44:10 -0800

    1 binaries uploaded

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
 import { Application } from '@nativescript/core';
-
+var explain =
+  'this is a marketplace designed for students to get helpful question banks for practice';
 Application.run({ moduleName: 'app-root' });
